a9e5efe201393c8bb47a4dc9602248318fe2cac7
Jeyush Pokhrel <jeyushpokhrel@gmail.com>
2025-07-09 22:17:40 +0545

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <nav> 
+      <img src='images/logo.png' className='logo' alt='logo' />
+      <ul>
+      <li>
+        <a href='#'>Home </a>
+      </li>
+
+      <li>
+        <a href='#'>Activity</a>
+      </li>
+
+      <li>
+        <a href='#'>About Us </a>
+      </li>
+
+
+      </ul>
+      <button className='btn' > 
+        <img src='images/icon.png'  alt='icon' />
+        Order
+      </button>
+     </nav>
+{/* { header content } */}
+
+    <div className='content'>
+      <h1>
+       Beautiful 
+        <br/>
+    Places to explore
+
+      </h1>
+       <p>
+    The sky turned a deep shade of orange as the sun began to set behind the mountains.<br />
+    A gentle breeze moved through the trees, rustling the leaves softly.<br />
+    Birds chirped in the distance, returning to their nests for the night.
+  </p>
+  {/* {form } */}
+  <form>
+    <input type='text' placeholder='Country Name' />
+    <button type='submit'className='btn'>
+      Search
+    </button>
+  </form>
+    </div>
+
     </div>
   );
 }
